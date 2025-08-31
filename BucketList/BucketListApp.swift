92f811c98f9d0a085c74776346c24755fc9adf4f
JoshuaRosado <joshua06rosado@gmail.com>
2025-08-31 16:20:37 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Joshua Rosado Olivencia on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
