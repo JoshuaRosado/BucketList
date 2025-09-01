8b797f9412bfdf8e486faf02ecefe77ece3a0822
JoshuaRosado <joshua06rosado@gmail.com>
2025-09-01 14:48:02 -0500

    Adding conformance to Comparable

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,15 +7,35 @@
 
 import SwiftUI
 
+struct User: Comparable, Identifiable{ // Add Comparable
+    let id = UUID()
+    let firstName:String
+    let lastName: String
+    
+    // lhs = left hand side
+    // rhs = right hand side
+    // < = operator over loaded
+    // static func = is called directly in the User struct itself
+    
+    static func <(lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+    
+        // Now that our User struct conforms to Identifiable and Comparable, we automatically get accesss to the parameter-less version of sorted()
+}
+
 struct ContentView: View {
+    let users = [
+        User(firstName: " Khai", lastName: "Asher"),
+        User(firstName: " Cataleya", lastName: "Grace"),
+        User(firstName: " Alexis", lastName: "Elizabeth")
+    ] // .sorted {
+    //$0.lastName < $1.lastName
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(users){ user in
+            Text("\(user.lastName),\(user.firstName)")
         }
-        .padding()
     }
 }
 
